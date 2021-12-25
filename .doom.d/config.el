ecff24bd9dbc8fad442593e4a7476e3ee82b7e61
AugustoRiedinger <riedinger.augusto@gmail.com>
2021-12-25 18:20:59 -0300

    Added configuration for yasnippets

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1,59 +1,16 @@
-;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
-
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
-
-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets.
+;; Identity config:
 (setq user-full-name "Augusto Riedinger"
       user-mail-address "riedinger.augusto@gmail.com")
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
-;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
-;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
-;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
+;; Setting themes:
 (setq doom-theme 'doom-one)
 
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
+;; Setting line numbers:
 (setq display-line-numbers-type t)
-
-
-;; Here are some additional functions/macros that could help you configure Doom:
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
-;; This will open documentation for it, including demos of how they are used.
-;;
-;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
-;; they are implemented.
-
 
 ;; Dashboard config:
 (use-package dashboard
-:init ;; tweak dashboard config before loading it
+:init
 (setq dashboard-set-heading-icons t)
 (setq dashboard-set-file-icons t)
 (setq dashboard-center-content t)
@@ -65,11 +22,11 @@
                         (projects . 3)
                         (registers . 3)))
 :config
-(dashboard-setup-startup-hook))
+(dashboard-setup-startup-hook)
 (setq initial-buffer-choice (lambda () (get-buffer "*dashboard*")))
 
 ;; Org mode config:
-(add-hook 'org-mode-hook 'org-indent-mode)
+(add-hook 'org-mode-hook 'org-indent-mode))
   (setq org-directory "~/org/"
         org-agenda-files '("~/org/agenda/")
         org-log-done 'time)
@@ -78,5 +35,13 @@
         org-src-tab-acts-natively t
         org-edit-src-content-indentation 0)
 
-(use-package org-bullets)
-(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+(use-package org-superstar
+  :config
+  (add-hook 'org-mode-hook (lambda () (org-superstar-mode 1))))
+
+;; yasnippet config:
+(use-package yasnippet
+  :config
+  (add-to-list 'load-path
+               "~/.doom.d/snippets/")
+  (yas-global-mode 1))
